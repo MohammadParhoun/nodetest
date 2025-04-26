ad35539bd09e1c76bea879c9b523167f410803ee
Mohammad Parhoun <mohammad.parhoun.95@gmail.com>
2025-04-26 22:03:49 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, Docker!');
+});
+
+server.listen(80, () => {
+  console.log('Server running at http://localhost:80/');
+});
+
