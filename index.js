f89d7a24560204c26a383174a46be8d88bdab13e
Mohammad Parhoun <mohammad.parhoun.95@gmail.com>
2025-06-08 16:25:24 +0330

    some changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Docker and Nodejs!!');
+  res.end('Hello, Docker and Nodejs!!!!');
 });
 
 server.listen(80, () => {
