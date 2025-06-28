978ee03310545c6019ac9ad0f325c6b307bf9b77
Mohammad Parhoun <mohammad.parhoun.95@gmail.com>
2025-06-28 10:17:46 +0330

    some changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Docker and Jenkins !!!');
+  res.end('Hello, Docker and Jenkins!!!');
 });
 
 server.listen(80, () => {
